36254cecd554788cfde20fa9a0f58476d2f86a3d
Olubusayo Amowe <amoweolubusayo@gmail.com>
2023-05-15 12:29:22 +0100

    fixed mounted position

--- packages/react-app/components/Navbar.tsx
+++ packages/react-app/components/Navbar.tsx
@@ -15,26 +15,26 @@ export default function Navbar() {
   }, []);
 
   return (
-    <>
     mounted && (
-      <header className="bg-white border-b-2 border-gray-100">
-        <nav
-          className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8"
-          aria-label="Top"
-        >
-          <div className="w-full py-6 flex flex-wrap items-center justify-between border-b border-yellow-500 lg:border-none">
-            <div className="flex items-center"></div>
-            <div className="ml-10 space-x-4 flex items-center">
-              {address ? (
-                <Navmenu address={address} disconnect={() => disconnect()} />
-              ) : (
-                <ConnectButton />
-              )}
+      <>
+        <header className="bg-white border-b-2 border-gray-100">
+          <nav
+            className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8"
+            aria-label="Top"
+          >
+            <div className="w-full py-6 flex flex-wrap items-center justify-between border-b border-yellow-500 lg:border-none">
+              <div className="flex items-center"></div>
+              <div className="ml-10 space-x-4 flex items-center">
+                {address ? (
+                  <Navmenu address={address} disconnect={() => disconnect()} />
+                ) : (
+                  <ConnectButton />
+                )}
+              </div>
             </div>
-          </div>
-        </nav>
-      </header>
+          </nav>
+        </header>
+      </>
     )
-    </>
   );
 }
